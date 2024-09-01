1e0cb5f57232612d82828d1fd66d4f126a34407f
Matrix4348 <127596209+Matrix4348@users.noreply.github.com>
2024-09-01 09:58:07 +0200

    Delete casualcollective.com lost game files Unfortunately, due to the php files, hosting them on github.io is useless.

--- lost-game-files/widget.casualcollective.com/load.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$gid = $_REQUEST['gid'];
-
-	echo 'zone=pub&w1=http://storage.cloud.casualcollective.com/zones/pub/' . $gid . '/widget.swf?PHPSESSID=0dg1k75uh2a5l4la0r48ua5a80&w2=http://widget.casualcollective.com';
